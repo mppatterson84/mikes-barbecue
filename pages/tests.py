d0c5864d607a0b2235abcd0a43d32e43d92b773d
mppatterson84 <mppatterson84@gmail.com>
2019-10-11 23:04:11 -0500

    Add pages tests.

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class PagesTests(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/home.html')
+
+    def test_about_page(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/about.html')
