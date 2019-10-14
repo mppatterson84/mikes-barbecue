3f2226419598b9338e23d9552c9159dd6c816cfe
mppatterson84 <mppatterson84@gmail.com>
2019-10-14 11:04:45 -0500

    Add styles.

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,31 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        context['home_active'] = 'active'
+        context['home_active_sr'] = '<span class="sr-only">(current)</span>'
+        return context
+    
+
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'About'
+        context['about_active'] = 'active'
+        context['about_active_sr'] = '<span class="sr-only">(current)</span>'
+        return context
+    
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contact'
+        context['contact_active'] = 'active'
+        context['contact_active_sr'] = '<span class="sr-only">(current)</span>'
+        return context
+    
