98335048ee130729e7491370c187006669bad631
mppatterson84 <mppatterson84@gmail.com>
2019-10-16 22:18:03 -0500

    Add parallax.

--- pages/views.py
+++ pages/views.py
@@ -22,4 +22,14 @@ class AboutPageView(TemplateView):
         context['about_active_link'] = '#'
         context['about_active_sr'] = '<span class="sr-only">(current)</span>'
         return context
-    
+
+class ParallaxPageView(TemplateView):
+    template_name = 'pages/parallax.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        context['home_active'] = 'active'
+        context['home_active_link'] = '#'
+        context['home_active_sr'] = '<span class="sr-only">(current)</span>'
+        return context
