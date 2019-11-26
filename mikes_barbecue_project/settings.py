dbfbea7a8f192b09cb1cd299fda8b7947c1ef408
mppatterson84 <mppatterson84@gmail.com>
2019-11-25 20:20:35 -0600

    Update ckeditor

--- mikes_barbecue_project/settings.py
+++ mikes_barbecue_project/settings.py
@@ -143,6 +143,32 @@ EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+CKEDITOR_BASE_PATH = "ckeditor/ckeditor/"
+CKEDITOR_CONFIGS = {
+    'default': {
+        'skin': 'moonocolor',
+        'extraPlugins': ['codesnippet', 'autogrow',],
+        'codeSnippet_theme': 'docco',
+        'autoGrow_onStartup': True,
+        'autoGrow_minHeight': '300',
+        'width': '100%',
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Source',],
+            ['Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo'],
+            ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt'],
+            ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv'],
+            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl'],
+            ['Link', 'Unlink'],
+            ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar'],
+            ['Styles', 'Format', 'Font', 'FontSize'],
+            ['TextColor', 'BGColor'],
+            ['Maximize', 'ShowBlocks'],
+            ['CodeSnippet'],
+        ],
+    }
+}
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
