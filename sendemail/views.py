83b43b0e5abec28f3eb28136b05332314b2ddd99
mppatterson84 <mppatterson84@gmail.com>
2020-12-29 07:50:54 -0600

    add reCAPTCHA verification

--- sendemail/views.py
+++ sendemail/views.py
@@ -1,9 +1,13 @@
+import os
+import requests
+
+from django.contrib import messages
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .forms import EmailForm
-import os
+
 
 def email_view(request):
     if request.method == 'GET':
@@ -11,15 +15,30 @@ def email_view(request):
     else:
         form = EmailForm(request.POST)
         if form.is_valid():
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            data = {
+                'secret': os.environ['RECAPTCHA_SECRET_KEY_V2'],
+                'response': recaptcha_response
+            }
+            r = requests.post(
+                'https://www.google.com/recaptcha/api/siteverify', data=data)
+            result = r.json()
             subject = form.cleaned_data['subject']
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
-            form.save()
-            try:
-                send_mail(subject, message, from_email, ['mppatterson84@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
+            if result['success']:
+                form.save()
+                try:
+                    send_mail(subject, message, from_email, [
+                        os.environ['SEND_EMAIL_ADDRESS']])
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found.')
+            else:
+                messages.error(
+                    request, 'Invalid reCAPTCHA. Please try again.', extra_tags='alert alert-warning')
+                return redirect('contact')
             return redirect('success')
+
     context = {
         'title': 'Contact Us',
         'contact_active': 'active',
@@ -33,7 +52,7 @@ def email_view(request):
 
 class SuccessPageView(TemplateView):
     template_name = 'sendemail/success.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Success'
